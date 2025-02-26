7a4ef4d2dcecdffe4597840543878bf5ccf05bba
gui123sparda <ribeiroguilherme72@gmail.com>
2025-02-26 10:34:14 -0300

    Update de UI

--- Assets/Scripts/DigimonController.cs
+++ Assets/Scripts/DigimonController.cs
@@ -1,4 +1,5 @@
 using Microsoft.Unity.VisualStudio.Editor;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,15 +8,24 @@ public class DigimonController : MonoBehaviour
     public EvolutionTree evolutionTree;
     public string digimonName;
     public Sprite digimonImage;
-
+    public SpriteRenderer spriteRenderer;
     public int treino = 0;
     public int felicidade =50;
     public int fome = 50;
 
+    public TextMeshProUGUI nomeTextMesh;
+    public TextMeshProUGUI treinoTextMesh;
+    public TextMeshProUGUI felicidadeTextMesh;
+    public TextMeshProUGUI fomeTextMesh;
+
+    void Awake(){
+        digimonName = evolutionTree.rootNode.digimonName;
+        digimonImage = evolutionTree.rootNode.digimonSprite;
+        spriteRenderer.sprite = digimonImage;
+    }
     void Start()
     {
         digimonName = evolutionTree.rootNode.digimonName;
-        digimonImage = evolutionTree.rootNode.digimonSprite;
         UpdateUI();
     }
 
@@ -40,6 +50,12 @@ public class DigimonController : MonoBehaviour
     void Update()
     {
         digimonName = evolutionTree.currentNode.digimonName;
+        spriteRenderer.sprite = digimonImage;
+        nomeTextMesh.text = digimonName;
+        treinoTextMesh.text = treino.ToString();
+        felicidadeTextMesh.text = felicidade.ToString();
+        fomeTextMesh.text = fome.ToString();
+        
         if(Input.GetKeyDown(KeyCode.Z)){
             Train();
         }
@@ -65,6 +81,7 @@ public class DigimonController : MonoBehaviour
 
     void UpdateUI()
     {
+        
         if (digimonImage == null || evolutionTree.currentNode.digimonSprite ==null){
             Debug.Log("Sem Imagem");
         }else
